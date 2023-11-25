db3630bf3e070aa2ec7588c571efb5a8a2fa3fc4
Ayush Singh <ayushsinghmi711@gmail.com>
2023-11-25 23:21:29 +0530

    App works

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Search from './components/Search/Search.jsx'
+import Table from './components/Table/Table'
+
 
 function App() {
 
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline bg-pink-600">
-      Hello world!
-    </h1>
-    </>
+    <div className='bg-slate-800
+    h-screen w-screen'>
+     <Header/> 
+     <Search/>
+     <Table/>
+    </div>
   )
 }
 
 export default App
+
+    // bg-slate-800 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <div className='flex  h-[50px] '>
     <h1 className='font-semibold text-3xl text-black bg-slate-200 h-[50px] mt-[25px] ml-[20px] p-[5px]
      border border-transparent rounded-xl hover:text-white hover:bg-slate-800'>
-    Ens Sights</h1>
+    EnsScan</h1>
   </div>
   )
 }
